706afb4b8c5e9676fe537cd4eb71216665882d17
Jared <jjohns3@tulane.edu>
2023-02-26 18:41:42 -0600

    fixed "only" test and refactored starting page into a hook

--- tests/azureswa.spec.js
+++ tests/azureswa.spec.js
@@ -1,69 +1,76 @@
 // This suite hits https://www.azurestaticwebapps.dev/
 
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require("@playwright/test");
 
-test('basic test', async ({ page }) => {
-  await page.goto('https://www.azurestaticwebapps.dev/');
-  const title = page.locator('.navbar__inner .navbar__title');
-  await expect(title).toHaveText('Static Web Apps!');
+test.beforeEach(async ({ page }) => {
+  await page.goto("https://www.azurestaticwebapps.dev/");
 });
 
-test.describe('navbar tests', () => {
-  test('the "#ThisMonthInSWA" navbar link goes to the "thismonth" page', async ({ page }) => {
-    await page.goto('https://www.azurestaticwebapps.dev/');
-    await page.getByText('#ThisMonthInSWA').click();
+test("basic test", async ({ page }) => {
+  const title = page.locator(".navbar__inner .navbar__title");
+  await expect(title).toHaveText("Static Web Apps!");
+});
+
+test.describe("navbar tests", () => {
+  test('the "#ThisMonthInSWA" navbar link goes to the "thismonth" page', async ({
+    page,
+  }) => {
+    await page.getByText("#ThisMonthInSWA").click();
     await expect(page).toHaveURL(/.*thismonth/);
   });
 
-  test('the "#30DaysOfSWA" navbar link goes to the "blog" page', async ({ page }) => {
-    await page.goto('https://www.azurestaticwebapps.dev/');
-    await page.getByText('#30DaysOfSWA').click();
+  test('the "#30DaysOfSWA" navbar link goes to the "blog" page', async ({
+    page,
+  }) => {
+    await page.getByText("#30DaysOfSWA").click();
     await expect(page).toHaveURL(/.*blog/);
   });
 
-  test('the "Roundup" navbar link goes to the "roundup" page', async ({ page }) => {
-    await page.goto('https://www.azurestaticwebapps.dev/');
-    await page.getByText('Roundup').click();
+  test('the "Roundup" navbar link goes to the "roundup" page', async ({
+    page,
+  }) => {
+    await page.getByText("Roundup").click();
     await expect(page).toHaveURL(/.*roundup/);
-  })
+  });
 
-  test('the "Roadmap" navbar link goes to the "roadmap" page', async ({ page }) => {
-    await page.goto('https://www.azurestaticwebapps.dev/');
-    await page.getByText('Roadmap').click();
+  test('the "Roadmap" navbar link goes to the "roadmap" page', async ({
+    page,
+  }) => {
+    await page.getByText("Roadmap").click();
     await expect(page).toHaveURL(/.*roadmap/);
-  })
+  });
 
   test('the "Docs" navbar link is present', async ({ page }) => {
-    await page.goto('https://www.azurestaticwebapps.dev/');
-    await expect(page.getByText('Docs')).toBeVisible();
-  })
+    await expect(page.getByText("Docs")).toBeVisible();
+  });
 
   test('the "Deploy" navbar link is present', async ({ page }) => {
-    await page.goto('https://www.azurestaticwebapps.dev/');
-    await expect(page.getByText('Deploy')).toBeVisible();
-  })
+    await expect(page.getByText("Deploy")).toBeVisible();
+  });
 
-  test('the Github navbar link is present', async ({ page }) => {
-    await page.goto('https://www.azurestaticwebapps.dev/');
-    await expect(page.getByRole('link', { name: "GitHub repository"})).toBeVisible();
-  })
+  test("the Github navbar link is present", async ({ page }) => {
+    await expect(
+      page.getByRole("link", { name: "GitHub repository" })
+    ).toBeVisible();
+  });
 
-  test.only('use navbar dark mode toggle to go to and from dark mode', async ({ page }) => {
-    await page.goto('https://www.azurestaticwebapps.dev/');
-    const toggleBtn = page.getByRole('button', { name: /.*dark and light/});
-    const lightIcon = page.locator('.lightToggleIcon_v35p');
-    const darkIcon = page.locator('.darkToggleIcon_nQuB');
-    await expect(toggleBtn).toHaveAttribute('title', /.*currently light mode/); // full title text is: Switch between dark and light mode (currently light mode)"
+  test("use navbar dark mode toggle to go to and from dark mode", async ({
+    page,
+  }) => {
+    const toggleBtn = page.getByRole("button", { name: /.*dark and light/ });
+    const lightIcon = page.locator(".lightToggleIcon_v35p");
+    const darkIcon = page.locator(".darkToggleIcon_nQuB");
+
+    await expect(toggleBtn).toHaveAttribute("title", /.*currently light mode/); // full title text is: Switch between dark and light mode (currently light mode)"
     await expect(lightIcon).toBeVisible();
 
     await toggleBtn.click();
-    await expect(toggleBtn).toHaveAttribute('title', /.*currently dark mode/); // full title text is: Switch between dark and light mode (currently dark mode)"
+    await expect(toggleBtn).toHaveAttribute("title", /.*currently dark mode/); // full title text is: Switch between dark and light mode (currently dark mode)"
     await expect(darkIcon).toBeVisible();
 
     await toggleBtn.click();
-    await expect(toggleBtn).toHaveAttribute('title', /.*currently light mode/); // full title text is: Switch between dark and light mode (currently light mode)"
+    await expect(toggleBtn).toHaveAttribute("title", /.*currently light mode/); // full title text is: Switch between dark and light mode (currently light mode)"
     await expect(lightIcon).toBeVisible();
-  })
-
-})
+  });
+});
